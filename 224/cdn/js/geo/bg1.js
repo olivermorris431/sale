7d6c33edb0df2b0e8faf72ad543008107b356d9c
olivermorris431 <64603316+olivermorris431@users.noreply.github.com>
2020-05-16 02:48:27 +0300

    Add files via upload

---
+++ 224/cdn/js/geo/bg1.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        bg: {
+            name: 'България',
+            code: 'bg',
+            price: 69,
+            oldPrice: 138,
+            labelPrice: 'leva',
+            phoneHelper: 'Например, +359 123456789',
+            nameHelper: 'Jivko Djurovic',
+            nameError: 'Името е задължително',
+            phoneError: 'Телефонният номер е задължителен',
+            countryError: 'Страната е задължителна',
+            isDefault: true
+        }
+    };
+    
+})();
